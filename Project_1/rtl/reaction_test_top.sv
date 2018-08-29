904ed706a5485c00a14605bc85817b208e511a67
DESKTOP-S0CCCTL\Jacob <mail@jboline.us>
2018-08-29 17:30:31 -0500

    Add constraints and a top level file structure.

---
+++ Project_1/rtl/reaction_test_top.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 08/29/2018 05:29:56 PM
+// Design Name: 
+// Module Name: reaction_test_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module reaction_test_top(
+
+    );
+endmodule
