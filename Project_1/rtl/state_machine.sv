7deeca8cf9ad9d5026bf05a8d2cc465a3c63c729
Jacob <mail@jboline.us>
2018-09-03 22:43:52 -0500

    Stopwatch counts up correctly after random time.

--- Project_1/rtl/state_machine.sv
+++ Project_1/rtl/state_machine.sv
@@ -10,11 +10,12 @@ module state_machine
 typedef enum logic [4:0] {init = 5'b00001, start = 5'b00010, delay = 5'b00100, count = 5'b01000} state_type;
 
 state_type curr_state, next_state;
-logic load_counter, clear_counter, enable_counter, zero_count;
-logic [3:0] random_val;
+logic load_counter, clear_counter, enable_counter, zero_count, timer_go;
+logic [3:0] random_val, timer_d0, timer_d1, timer_d2, timer_d3;
 
 univ_bin_counter #(.N(32)) counter(.clk(clk), .reset(rst), .syn_clr(clear_counter), .load(load_counter), .en(enable_counter), .up(0), .d(100000000*random_val), .max_tick(), .min_tick(zero_count), .q());
 LFSR_fib168pi rng(.clk(clk), .reset(rst), .seed(28'b1110_1100_0111_1110_0111_0110_1100), .r(random_val));
+stop_watch_if stop_watch(.clk(clk), .go(timer_go), .clr(rst), .d3(timer_d3), .d2(timer_d2), .d1(timer_d1), .d0(timer_d0));
 
 always_ff @(posedge clk, posedge rst) begin
     if(rst == 1'b1) begin
@@ -36,6 +37,7 @@ always_comb begin
                 digit2 = 4'b1111;
                 digit3 = 4'b1111;
                 led0 = 0;
+                timer_go = 0;
                 
                 enable_counter = 0;
                 load_counter = 0;
@@ -53,6 +55,7 @@ always_comb begin
                 digit2 = 4'b1111;
                 digit3 = 4'b1111;
                 led0 = 0;
+                timer_go = 0;
                 
                 enable_counter = 0;
                 clear_counter = 0;
@@ -68,6 +71,7 @@ always_comb begin
                 digit2 = 4'b1111;
                 digit3 = 4'b1111;
                 led0 = 0;
+                timer_go = 0;
             
                 load_counter = 0;
                 enable_counter = 1;
@@ -79,11 +83,12 @@ always_comb begin
         count:
             begin
                 ltr_flag = 0;
-                digit0 = 4'b1111;
-                digit1 = 4'b1111;
-                digit2 = 4'b1111;
-                digit3 = 4'b1111;
                 led0 = 1;
+                timer_go = 1;
+                digit0 = timer_d0;
+                digit1 = timer_d1;
+                digit2 = timer_d2;
+                digit3 = timer_d3;
             
                 load_counter = 0;
                 enable_counter = 0;
@@ -102,6 +107,7 @@ always_comb begin
                 digit2 = 4'b1111;
                 digit3 = 4'b1111;
                 led0 = 0;
+                timer_go = 0;
                 
                 load_counter = 0;
                 enable_counter = 0;
